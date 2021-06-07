a41196816dd979971184d537f8688cf702f2701b
Anusha-11 <72800298+Anusha-11@users.noreply.github.com>
2021-06-07 04:41:51 -0700

    Add files via upload

---
+++ Note.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div className="note">
+      <h1>Javascript and React.js</h1>
+      <p>
+        Hello Sir, I am Anusha Kanthed. I am thanking you for taking such a
+        wonderful Bootcamp.
+      </p>
+    </div>
+  );
+}
+export default Note;
